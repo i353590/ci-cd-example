77421ceeeccca48be2249c8dedaa4e98820fee29
Shubham Agarwal <shubham.agarwal@sap.com>
2021-05-22 19:17:51 +0530

    Config File Bucket name corrected

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the Cloud1
+          I AM Sample App Developed - by Team 5
         </div>
       </div>
     );
