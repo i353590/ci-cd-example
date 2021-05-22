38fe4b393d09d870f0ff86ba259894ecc7839197
Shubham Agarwal <shubham.agarwal@sap.com>
2021-05-22 20:49:30 +0530

    TestJs changed

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/example app/i);
+  //const linkElement = screen.getByText(/example app/i);
+  const linkElement = el.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
